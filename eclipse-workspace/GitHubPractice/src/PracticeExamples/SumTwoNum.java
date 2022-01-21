186cb4834dddd81cd92e0c81e432b64b53a2e12e
User <pbendadi@gmail.com>
2022-01-20 21:04:40 -0600

    first push to master branch

---
+++ eclipse-workspace/GitHubPractice/src/PracticeExamples/SumTwoNum.java
@@ -0,0 +1,24 @@
+package PracticeExamples;
+
+public class SumTwoNum {
+
+	public static void main(String[] args) {
+		int D,E,F; // lince of code
+		 D=150;
+		//Read E
+		 E=50;
+		
+		//print result
+		 F=D-E;
+		System.out.println(F);
+	}
+// Subtraction - sana
+	
+	
+// MUltiplication -- Nandana
+}
+
+
+// if we are creating similar datatype  int a, b,c;
+// variable - used to sto store a value
+// variable name suould be small case letters.
